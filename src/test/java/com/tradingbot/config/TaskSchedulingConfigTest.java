f54e6a3ac6db3a52f8395c3a1a0574eaa1f560f6
sharad prasoon <sharad.prasoon@gmail.co.in>
2026-09-18 16:53:07 +0530

    feat(config): configure ThreadPoolTaskScheduler with pool size of 5

---
+++ src/test/java/com/tradingbot/config/TaskSchedulingConfigTest.java
@@ -0,0 +1,22 @@
+package com.tradingbot.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@SpringBootTest
+public class TaskSchedulingConfigTest {
+
+    @Autowired
+    private ThreadPoolTaskScheduler taskScheduler;
+
+    @Test
+    void testTaskSchedulerPoolSize() {
+        assertNotNull(taskScheduler);
+        assertEquals(5, taskScheduler.getPoolSize());
+        assertEquals("trading-task-", taskScheduler.getThreadNamePrefix());
+    }
+}
